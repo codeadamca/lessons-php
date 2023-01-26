46d67a20f22ca83a6618ea975c335a78c5e0193e
Adam Thomas <thomasadam83@hotmail.com>
2023-01-26 15:39:18 -0500

    Added if samples

---
+++ week-3-if/if.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP If Statements</title>
+</head>
+<body>
+
+    <h1>PHP If Statements</h1>
+
+    <?php
+
+    $first_name = "Adam";
+
+    if($first_name == "")
+    {
+
+    }
+
+    $last_name = "Thomas";
+
+    // If $last_name has ANY value, it will evaluate as true
+    if($last_name)
+    {
+
+    }
+
+    // Not having a value
+    $email;
+
+    // This would evaluate to false
+    if($email)
+    {
+
+    }
+
+    // Undefined
+    // This would also evaluate to false
+    if($postal_code)
+    {
+
+    }
+
+
+    ?>
+    
+</body>
+</html>
